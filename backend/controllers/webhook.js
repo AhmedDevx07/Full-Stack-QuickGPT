b71d61bf1483ada7d74609a4bacad1ab6c8ca4c2
Ahmed-Devx <ahmedoffice69@gmail.com>
2026-06-24 18:05:14 +0500

    Set Error

--- backend/controllers/webhook.js
+++ backend/controllers/webhook.js
@@ -22,7 +22,7 @@ export const stripeWebhooks = async (request, response) => {
           payment_intent: paymentIntent.id,
         });
         const session = sessionList.data[0];
-        const { transactionId, appId = session.metadata };
+        const { transactionId, appId} = session.metadata ;
         if (appId === "quickgpt") {
           const transaction = await Transaction.findOne({
             _id: transactionId,
